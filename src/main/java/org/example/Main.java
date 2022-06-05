dec3f026d53cccc3b2bc13656a03734385f28244
student <student@itpark>
2022-06-05 18:31:55 +0300

    exposed port 7777

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        final Server server = new Server();
-        server.setPort(7777);
-        server.start();
-    }
-}
